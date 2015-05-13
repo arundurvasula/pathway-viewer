0a63d815f8cf81a7c0905092f167d74edd9c8669
Arun Durvasula <ohblahitsme@yahoo.com>
2015-05-12 18:17:27 -0700

    closes #6. adds pathway names

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ def get_pathway(gene):
 	first_gene = first_line.split("\t")[0]
 	gene = requests.get('http://rest.kegg.jp/get/'+first_gene).content.split("\n")
 	pathway = filter(lambda x:'PATHWAY' in x, gene)[0].split("\t")[0].split(" ")[5]
-	return pathway, first_gene
+	pathway_name = filter(lambda x:'PATHWAY' in x, gene)[0].split("\t")[0].split(" ")[7:]
+	pathway_name = " ".join(pathway_name)
+	return pathway, first_gene, pathway_name
 
 @app.route("/")
 def index():
@@ -27,20 +29,23 @@ def data():
 	finds the pathway and the genes
 	"""
 	multiple_genes = False
+	data = ""
 	if "\n" not in flask.request.form['gene-names']:
 		data = flask.request.form['gene-names']
-		pathway, gene = get_pathway(data)
+		pathway, gene, pathway_name = get_pathway(data)
 	else:
 		multiple_genes = True
 		data = flask.request.form['gene-names'].split("\n")
 		pathway = []
 		gene = []
+		pathway_name = []
 		for entry in data:
-			p, g = get_pathway(entry)
+			p, g, pn = get_pathway(entry)
 			pathway.append(p)
 			gene.append(g)
+			pathway_name.append(pn)
 
-	return flask.render_template("data.html", multiple_genes=multiple_genes, pathway=pathway, genes=gene)
+	return flask.render_template("data.html", multiple_genes=multiple_genes, pathway=pathway, genes=gene, genes_req=data, pathway_name=pathway_name)
 
 if __name__ == "__main__":
     import os
